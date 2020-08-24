441c360981ccfbc724bf1a02d11064ad716ecc49
venkateswaran95 <95venkateswaran@gmail.com>
2020-08-24 10:55:38 +0530

    changes in UI

--- www/controller/Home.controller.js
+++ www/controller/Home.controller.js
@@ -2,21 +2,44 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
+	
+	var that;
 
 	return Controller.extend("QRCodeScanner.controller.Home", {
 		onInit: function(){
 			
 		},
 		onScanClick: function(){
+			that=this;
 			  cordova.plugins.barcodeScanner.scan(
 				  function (result) {
-					  alert("We got a barcode\n" +
+				/*	  alert("We got a barcode\n" +
 					  "Result: " + result.text + "\n" +
 					  "Format: " + result.format + "\n" +
-					  "Cancelled: " + result.cancelled);
+					  "Cancelled: " + result.cancelled);*/
+					 that.getView().byId("result").setText(result.text);
+					 that.getView().byId("format").setText(result.format);
+					 sap.m.MessageToast.show("We got a barcode\n" +
+					  "Result: " + result.text + "\n" +
+					  "Format: " + result.format + "\n" +
+					  "Cancelled: " + result.cancelled, {
+									duration: 3000,                  // default
+									width: "15em",                   // default
+									my: "center bottom",             // default
+									at: "center bottom",             // default
+									of: window,                      // default
+									offset: "0 0",                   // default
+									collision: "fit fit",            // default
+									onClose: null,                   // default
+									autoClose: true,                 // default
+									animationTimingFunction: "ease", // default
+									animationDuration: 1000,         // default
+									closeOnBrowserNavigation: true   // default
+				});
 				  },
 				  function (error) {
-					alert("Scanning failed: " + error);
+				
+					sap.m.MessageToast.show("Scanning Failed");
 				  }
 			  );
 		}
